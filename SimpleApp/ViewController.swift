85d563725005aecd598cc1965080d6bafefb4f74
rivera191 <cristianrivera096@gmail.com>
2019-01-02 09:50:37 -0500

    made label hidden
    Made the label hidden until user put name

--- SimpleApp/ViewController.swift
+++ SimpleApp/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     let label: UILabel = UILabel();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,14 +21,17 @@ class ViewController: UIViewController {
         label.frame.origin = CGPoint(x: 50, y: 300);  //frame is a CGRect.
         label.frame.size = label.intrinsicContentSize; //needed if not using constraints
         print("label.frame = \(label.frame)");         //(100.0, 100.0, 169.0, 61.0)
+        label.isHidden  = true;
         print(view.subviews.count);
         view.addSubview(label);
         print(view.subviews.count);
     }
+    
     @IBAction func returnedInput(_ sender: UITextField) {
         sender.resignFirstResponder();
-        let name:String = sender.text!
-        label.text = "Welcome \(name) swipe any\n direction on your screen "
+        //let name:String = sender.text!
+        label.text = "Welcome \(sender.text!) swipe any\n direction on your screen "
+        label.isHidden = false;
     }
     
     @IBAction func swipeUp(_ sender: UISwipeGestureRecognizer) {
